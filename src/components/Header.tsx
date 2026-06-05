3f9589cffb46ce970d5e03cc1d3f85ada97376d4
zebigode0000 <teixeiraqugusto@gmail.com>
2026-06-05 18:35:47 -0300

    feat and style: transactions table created

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export function Header() {
     return (
         <div className="bg-base-background w-full pt-10 pb-30">
-            <div className="max-w-[1120px] mx-auto px-6 flex justify-between items-center">
+            <div className="MainContainer flex justify-between items-center">
                 <h1>DT Money</h1>
 
                 <button className="h-[50px] border-0 bg-green text-white font-bold px-4 transition-all rounded-sm cursor-pointer hover:bg-green-dark">Nova Transação</button>
--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -1,6 +1,31 @@
+import { ArrowCircleDownIcon, ArrowCircleUpIcon, CurrencyDollarIcon } from "@phosphor-icons/react";
+
 export function Summary() {
     return (
-        <div>
+        <div className="MainContainer grid grid-cols-3 gap-4 mt-[-5rem]">
+            <div className="SummaryCard">
+                <header className="HeaderSummaryCard">
+                    <span>Entradas</span>
+                    <ArrowCircleUpIcon size={32} className="text-green-light" />
+                </header>
+                <strong className="StrongSummaryCard">R$ 17.400,00</strong>
+            </div>
+
+            <div className="SummaryCard">
+                <header className="HeaderSummaryCard">
+                    <span>Saidas</span>
+                    <ArrowCircleDownIcon size={32} className="text-red-400" />
+                </header>
+                <strong className="StrongSummaryCard">R$ 17.400,00</strong>
+            </div>
+
+            <div className="SummaryCard bg-green-dark">
+                <header className="HeaderSummaryCard">
+                    <span>Total</span>
+                    <CurrencyDollarIcon size={32} className="text-green-light" />
+                </header>
+                <strong className="StrongSummaryCard">R$ 17.400,00</strong>
+            </div>
             
         </div>
     )
