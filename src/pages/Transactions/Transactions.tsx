f8df1ca22149d5110736e4df645b2544311c453a
zebigode0000 <teixeiraqugusto@gmail.com>
2026-06-15 14:29:53 -0300

    refactor style

--- src/pages/Transactions/Transactions.tsx
+++ src/pages/Transactions/Transactions.tsx
@@ -13,37 +13,40 @@ export function Transactions() {
     })
 
   return (
-    <div className="MainContainer mt-12 px-4 md:px-0 max-w-full overflow-x-hidden">
+    <div>
       <Header />
       <Summary />
 
-      <div className="w-full max-w-full overflow-x-auto mt-5">
+      <div className="MainContainer mt-12 px-4 md:px-0">
         <SearchForm />
-        <table className="w-full min-w-[600px] [border-collapse:separate] [border-spacing:0_0.5rem] mt-5">
-          <tbody className="w-full">
-            {transactions.map((item) => (
-              <tr key={item.id}>
-                <td className="TdTableTransactions rounded-tl-md rounded-bl-md">
-                  {item.description}
-                </td>
-                <td
-                  className={`TdTableTransactions ${
-                    item.type === "income"
-                      ? "text-green"
-                      : "text-red"
-                  }`}
-                >
-                  {item.type === "outcome" && "- "}
-                  {priceFormatter.format(item.price)}
-                </td>
-                <td className="TdTableTransactions">{item.category}</td>
-                <td className="TdTableTransactions rounded-tr-md rounded-br-md">
-                  {dateFormatter.format(new Date(item.createdAt))}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+
+        <div className="w-full overflow-x-auto mt-5">
+          <table className="w-full min-w-[600px] [border-collapse:separate] [border-spacing:0_0.5rem] mt-5">
+            <tbody className="w-full">
+              {transactions.map((item) => (
+                <tr key={item.id}>
+                  <td className="TdTableTransactions rounded-tl-md rounded-bl-md">
+                    {item.description}
+                  </td>
+                  <td
+                    className={`TdTableTransactions ${
+                      item.type === "income"
+                        ? "text-green"
+                        : "text-red"
+                    }`}
+                  >
+                    {item.type === "outcome" && "- "}
+                    {priceFormatter.format(item.price)}
+                  </td>
+                  <td className="TdTableTransactions">{item.category}</td>
+                  <td className="TdTableTransactions rounded-tr-md rounded-br-md">
+                    {dateFormatter.format(new Date(item.createdAt))}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
